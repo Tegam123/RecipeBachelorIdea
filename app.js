59e2c703d18c2d088ea12f1baa7f56e4e2f43ed7
Tegam123 <47597872+Tegam123@users.noreply.github.com>
2021-02-19 09:05:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+ 
+const app = express(); 
+
+
+//Middleware 
+
+
+// Routes 
+app.get('/',(req, res) => {
+    res.send("Something");
+})
+
+app.get('/Opskrifter', (req, res) => {
+    res.send('Her er nogle opskrifter...')
+})
+
+// How to listen 
+app.listen(3000); 
