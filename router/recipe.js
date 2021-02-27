ad6b83eac0f12ac988d285f185f467efdf3de26b
VictorKildahl <victor.petersen2@gmail.com>
2021-02-27 18:58:16 +0100

    added endpoint

--- router/recipe.js
+++ router/recipe.js
@@ -45,6 +45,41 @@ const Role = require("_helpers/role");
  */
 router.get("/getrecipes", recipeController.getRecipes); // public route
 
+////////VIRKER IKKE ENDNU//////////
+////////VIRKER IKKE ENDNU//////////
+////////VIRKER IKKE ENDNU//////////
+////////VIRKER IKKE ENDNU//////////
+/**
+ * @swagger
+ * /recipes/createrecipe:
+ *   post:
+ *     summary: Create a recipe.
+ *     tags: [Recipes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 required: true
+ *               ingrediens:
+ *                 type: array
+ *                 required: true
+ *                 items:
+ *                   type: string
+ *               description:
+ *                 type: string
+ *                 required: true
+ *               category:
+ *                 type: string
+ *                 required: true
+ *     responses:
+ *       200:
+ *         description: Created recipe
+ */
 router.post(
   "/createrecipe",
   authorize([Role.User, Role.Administrator, Role.Manager]),
